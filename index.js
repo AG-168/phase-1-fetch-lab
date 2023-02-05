874161301df4e61452506cd3b47d56e390dba71b
Anson Guo <anson.guo.921104@gmail.com>
2023-02-05 14:20:44 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => {return resp.json()})
+  .then(data => renderBooks(data))
   
 }
 
